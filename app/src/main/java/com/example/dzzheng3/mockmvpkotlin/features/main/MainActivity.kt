fb530518460e288cc58038e8bf5d6da378333145
dzzheng3 <150183186@qq.com>
2018-03-31 19:46:30 -0700

    create

---
+++ app/src/main/java/com/example/dzzheng3/mockmvpkotlin/features/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dzzheng3.mockmvpkotlin.features.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.dzzheng3.mockmvpkotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
